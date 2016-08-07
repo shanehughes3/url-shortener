cb8c707a6b45c4bbe4ca2dce91e4f0fd6d7eda8a
Shane Hughes <shanehughes314@gmail.com>
2016-08-07 16:27:21 -0500

    added env vars for deployment on heroku

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 module.exports = {
-    db: "mongodb://localhost/url-shortener",
-    serverURL: "http://localhost/"
+    db: process.env.MONGOLAB_URI || "mongodb://localhost/url-shortener",
+    serverURL: process.env.SERVER_URI || "http://localhost/"
 }
